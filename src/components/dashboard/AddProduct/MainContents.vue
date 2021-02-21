8e817bbef5627eb01555ef212eca31b5ab82eb17
Michael Kabutey <michael@Michaels-Mac-mini.local>
2021-02-21 03:24:05 +0000

    #Dashboard: Add product page for dynamics done

--- src/components/dashboard/AddProduct/MainContents.vue
+++ src/components/dashboard/AddProduct/MainContents.vue
@@ -28,7 +28,7 @@
         <!-- Begin form section -->
         <section class="px-6">
             <div class="">
-                <form class="">
+                <form @submit.prevent="" class="">
 
                     <!-- Begin basic info -->
                     <div class="bg-white rounded mb-5 p-5">
@@ -156,7 +156,7 @@
 
                             <!-- Begin add button -->
                             <div class="text-xs">
-                                <button class="flex items-center rounded bg-juaso-primary text-white font-bold px-5 py-2 w-full focus:outline-none hover:bg-juaso-secondary">
+                                <button @click="addSpecification" class="flex items-center rounded bg-juaso-primary text-white font-bold px-5 py-2 w-full focus:outline-none hover:bg-juaso-secondary">
                                     <svg class="w-3 h-3 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z"></path></svg>
                                     <span class="uppercase text-xxxs font-bold">Add more</span>
                                 </button>
@@ -167,7 +167,37 @@
                         <!-- End header contents -->
 
                         <!-- Begin form -->
-                        <div class="flex grid grid-cols-2 gap-5 justify-evenly py-4 text-xs">
+                        <div v-for="( spec, index ) in specifications" :key="index" class="flex grid grid-cols-2 gap-5 justify-evenly items-center py-1 text-xs">
+
+                            <!-- Begin specification -->
+                            <div class="mt-1">
+                                <label class="text-gray-500 text-xs">Specification</label>
+                                <div class="flex mt-1">
+                                    <div class="w-12 z-10 text-center pointer-events-none flex items-center justify-center border-l border-t border-b rounded-l">
+                                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"></path></svg>
+                                    </div>
+                                    <input type="text" v-model="spec.specification" class="border rounded-r px-3 py-2 w-full focus:outline-none" placeholder="Specification">
+                                </div>
+                            </div>
+                            <!-- End specification -->
+
+                            <!-- Begin value -->
+                            <div class="mt-1">
+                                <label class="text-gray-500 text-xs">Value</label>
+                                <div class="flex mt-1">
+                                    <div class="w-12 z-10 text-center pointer-events-none flex items-center justify-center border-l border-t border-b rounded-l">
+                                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"></path></svg>
+                                    </div>
+                                    <input type="text" v-model="spec.value" class="border rounded-r px-3 py-2 w-full focus:outline-none" placeholder="Value">
+                                    <div class="text-xs ml-4">
+                                        <button v-on:click="removeSpecification( index );" class="flex items-center rounded bg-red-500 text-white font-bold px-5 py-3 w-full focus:outline-none hover:bg-red-600">
+                                            <svg class="w-3 h-3 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z"></path></svg>
+                                            <span class="uppercase text-xxxs font-bold">Remove</span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End value -->
 
                         </div>
                         <!-- End form -->
@@ -189,7 +219,7 @@
 
                             <!-- Begin add button -->
                             <div class="text-xs">
-                                <button class="flex items-center rounded bg-juaso-primary text-white font-bold px-5 py-2 w-full focus:outline-none hover:bg-juaso-secondary">
+                                <button @click="addOverview" class="flex items-center rounded bg-juaso-primary text-white font-bold px-5 py-2 w-full focus:outline-none hover:bg-juaso-secondary">
                                     <svg class="w-3 h-3 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z"></path></svg>
                                     <span class="uppercase text-xxxs font-bold">Add more</span>
                                 </button>
@@ -200,7 +230,49 @@
                         <!-- End header contents -->
 
                         <!-- Begin form -->
-                        <div class="flex grid grid-cols-2 gap-5 justify-evenly py-4 text-xs">
+                        <div v-for="( overview, index ) in overviews" :key="index" class="flex grid grid-cols-3 gap-5 justify-evenly py-2 text-xs">
+
+                            <!-- Begin title -->
+                            <div class="mt-1">
+                                <label class="text-gray-500 text-xs">Title</label>
+                                <div class="flex mt-1">
+                                    <div class="w-12 z-10 text-center pointer-events-none flex items-center justify-center border-l border-t border-b rounded-l">
+                                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"></path></svg>
+                                    </div>
+                                    <input type="text" v-model="overview.title" class="border rounded-r px-3 py-2 w-full focus:outline-none" placeholder="Title">
+                                </div>
+                            </div>
+                            <!-- End title -->
+
+                            <!-- Begin description -->
+                            <div class="mt-1">
+                                <label class="text-gray-500 text-xs">Description</label>
+                                <div class="flex mt-1">
+                                    <div class="w-12 z-10 text-center pointer-events-none flex items-center justify-center border-l border-t border-b rounded-l">
+                                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"></path></svg>
+                                    </div>
+                                    <input type="text" v-model="overview.description" class="border rounded-r px-3 py-2 w-full focus:outline-none" placeholder="Description">
+                                </div>
+                            </div>
+                            <!-- End description -->
+
+                            <!-- Begin image -->
+                            <div class="mt-1">
+                                <label class="text-gray-500 text-xs">File (Image)</label>
+                                <div class="flex mt-1">
+                                    <div class="w-12 z-10 text-center pointer-events-none flex items-center justify-center border-l border-t border-b rounded-l">
+                                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"></path></svg>
+                                    </div>
+                                    <input type="file" @change="setOverviewFilename( $event, overview )" :id="index" class="border rounded-r px-3 py-2 w-full focus:outline-none" placeholder="Choose file">
+                                    <div class="text-xs ml-4">
+                                        <button v-on:click="removeOverview( index );" class="flex items-center rounded bg-red-500 text-white font-bold px-5 py-3 w-full focus:outline-none hover:bg-red-600">
+                                            <svg class="w-3 h-3 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z"></path></svg>
+                                            <span class="uppercase text-xxxs font-bold">Remove</span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End image -->
 
                         </div>
                         <!-- End form -->
@@ -222,7 +294,7 @@
 
                             <!-- Begin add button -->
                             <div class="text-xs">
-                                <button class="flex items-center rounded bg-juaso-primary text-white font-bold px-5 py-2 w-full focus:outline-none hover:bg-juaso-secondary">
+                                <button @click="addFile" class="flex items-center rounded bg-juaso-primary text-white font-bold px-5 py-2 w-full focus:outline-none hover:bg-juaso-secondary">
                                     <svg class="w-3 h-3 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z"></path></svg>
                                     <span class="uppercase text-xxxs font-bold">Add more</span>
                                 </button>
@@ -233,7 +305,37 @@
                         <!-- End header contents -->
 
                         <!-- Begin form -->
-                        <div class="flex grid grid-cols-2 gap-5 justify-evenly py-4 text-xs">
+                        <div v-for="( file, index ) in files" :key="index" class="flex grid grid-cols-2 gap-5 justify-evenly py-2 text-xs">
+
+                            <!-- Begin description -->
+                            <div class="mt-1">
+                                <label class="text-gray-500 text-xs">Description</label>
+                                <div class="flex mt-1">
+                                    <div class="w-12 z-10 text-center pointer-events-none flex items-center justify-center border-l border-t border-b rounded-l">
+                                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"></path></svg>
+                                    </div>
+                                    <input type="text" v-model="file.description" class="border rounded-r px-3 py-2 w-full focus:outline-none" placeholder="Description">
+                                </div>
+                            </div>
+                            <!-- End description -->
+
+                            <!-- Begin image -->
+                            <div class="mt-1">
+                                <label class="text-gray-500 text-xs">File (Image)</label>
+                                <div class="flex mt-1">
+                                    <div class="w-12 z-10 text-center pointer-events-none flex items-center justify-center border-l border-t border-b rounded-l">
+                                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"></path></svg>
+                                    </div>
+                                    <input type="file" @change="setFilename( $event, file )" :id="index" class="border rounded-r px-3 py-2 w-full focus:outline-none" placeholder="Choose file">
+                                    <div class="text-xs ml-4">
+                                        <button v-on:click="removeFile( index );" class="flex items-center rounded bg-red-500 text-white font-bold px-5 py-3 w-full focus:outline-none hover:bg-red-600">
+                                            <svg class="w-3 h-3 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z"></path></svg>
+                                            <span class="uppercase text-xxxs font-bold">Remove</span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End image -->
 
                         </div>
                         <!-- End form -->
@@ -257,7 +359,71 @@
     export default
     {
         name: "MainContents",
-      components: { TopNavBar }
+        components: { TopNavBar },
+        data()
+        {
+            return {
+                specifications: [],
+                overviews: [],
+                files: []
+            }
+        },
+
+        methods :
+        {
+            addSpecification()
+            {
+                this.specifications.push(
+                {
+                    type: "Specification",
+                    specification: "",
+                    value: "",
+                });
+            },
+            removeSpecification: function( index )
+            {
+                this.specifications.splice( index, 1 );
+            },
+
+            addOverview()
+            {
+                this.overviews.push(
+                {
+                    type: "Overview",
+                    title: "",
+                    description: "",
+                    file_path: null
+                });
+            },
+            removeOverview: function( index )
+            {
+                this.overviews.splice( index, 1 );
+            },
+            setOverviewFilename: function( event, overview )
+            {
+                let file_path = event.target.files[0];
+                overview.file_path = file_path
+            },
+
+            addFile: function ()
+            {
+                this.files.push(
+                {
+                    type: "File",
+                    description: "",
+                    file: null
+                });
+            },
+            removeFile: function( index )
+            {
+                this.files.splice( index, 1 );
+            },
+            setFilename: function( event, fileIndex )
+            {
+              let file = event.target.files[0];
+                fileIndex.file = file
+            },
+        }
     }
 </script>
 
---
+++ src/components/dashboard/Dashboard/BasicStats.vue
@@ -0,0 +1,81 @@
+<template>
+
+    <!-- Begin basic stats -->
+    <section class="px-6 mb-9">
+        <div class="bg-white shadow rounded text-gray-600">
+
+            <!-- Begin heading -->
+            <div class="font-bold text-lg px-5 py-3">Monthly Overview</div>
+            <!-- End heading -->
+
+            <hr class="border-gray-100">
+
+            <!-- Begin stats contents -->
+            <div class="flex grid grid-cols-4 px-5 py-3">
+
+                <!-- Begin product counts -->
+                <div class="flex items-center py-2">
+                    <div class="mr-3">
+                        <svg class="w-14 h-14" style="margin-left: -11px" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path></svg>
+                    </div>
+                    <div>
+                        <h4 class="text-2xl">30,000</h4>
+                        <p class="text-xs">Total products online</p>
+                    </div>
+                </div>
+                <!-- End product counts -->
+
+                <!-- Begin product sold -->
+                <div class="flex items-center py-2">
+                    <div class="mr-3">
+                        <svg class="w-14 h-14" style="margin-left: -11px" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd"></path></svg>
+                    </div>
+                    <div>
+                        <h4 class="text-2xl">300,000</h4>
+                        <p class="text-xs">Total products sold</p>
+                    </div>
+                </div>
+                <!-- End product sold -->
+
+                <!-- Begin product orders -->
+                <div class="flex items-center py-2">
+                    <div class="mr-3">
+                        <svg class="w-14 h-14" style="margin-left: -11px" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path></svg>
+                    </div>
+                    <div>
+                        <h4 class="text-2xl">30,000</h4>
+                        <p class="text-xs">Total orders</p>
+                    </div>
+                </div>
+                <!-- End product orders -->
+
+                <!-- Begin customers counts -->
+                <div class="flex items-center py-2">
+                    <div class="mr-3">
+                        <svg class="w-14 h-14" style="margin-left: -11px"  fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"></path></svg>
+                    </div>
+                    <div>
+                        <h4 class="text-2xl">30,000</h4>
+                        <p class="text-xs">Total customers</p>
+                    </div>
+                </div>
+                <!-- End customers counts -->
+
+            </div>
+            <!-- End stats contents -->
+
+        </div>
+    </section>
+    <!-- End basic stats -->
+
+</template>
+
+<script>
+    export default
+    {
+        name: "BasicStats"
+    }
+</script>
+
+<style scoped>
+</style>
